51750d977050b95e6e5ef441eec79ba0a9b20fea
Timothy McGowen <mcgowen17@yahoo.com>
2019-10-08 23:51:43 -0700

    Added common elements challenge

---
+++ commonElements.js
@@ -0,0 +1,19 @@
+// Good morning! Write a function called commonElements that has parameters for two arrays.  Return an array of all items that are present in both arrays.  Do not modify the arrays that are passed in.
+
+function commonElements(arr1, arr2) {
+    let arr = []
+    arr1.map(e => {
+        let arr3 = arr2.map(f => {
+            if (e === f) {
+                arr.push(e)
+            }
+        })
+    })
+    return arr;
+}
+
+arr1 = [1, 2, 3, 4, 7, 8]
+arr2 = [3, 4, 5, 6, 7, 8]
+
+
+console.log(commonElements(arr1, arr2))
